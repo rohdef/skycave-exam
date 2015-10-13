c80ef3491bef073ad5b20e2d698990a69f2879f5
Rohde Fischer <rohdef@rohdef.dk>
2015-10-13 13:32:57 +0200

    mongo

--- start-mongo.sh
+++ start-mongo.sh
@@ -3,5 +3,5 @@
 cd /root/cave
 source setupLocalSocket.sh
 export SKYCAVE_CAVESTORAGE_IMPLEMENTATION=cloud.cave.server.service.ServerCaveStorage
-export SKYCAVE_DBSERVER=172.17.0.17:27017
+export SKYCAVE_DBSERVER=db0:27017
 ant ocean-daemon
